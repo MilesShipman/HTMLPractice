127d1da8cd7971e6d8c49f764198430bcbb7aed3
Miles Shipman <mshipman3@gatech.edu>
2017-07-22 23:22:58 -0400

    Can change the month

--- js/calendar.js
+++ js/calendar.js
@@ -5,21 +5,40 @@ Date.prototype.getMonthText = function() {
 
 calendarDays = Array(42).fill(0);
 
+date = new Date()
+
+
+function setup(){
+  //Create Calendar if it exists
+  var items = document.getElementsByClassName("calendar");
+  if(items.length > 0){
+    initial_calendar_create();
+  }
+}
+
+function initial_calendar_create(){
+  //Get the Current Day and set the new month
+  var firstDay = new Date(date.getFullYear(), date.getMonth(),1);
+  date = firstDay;
+
+  create_calendar();
+}
+
 function create_calendar(){
+  //Reset Array
+  reset_days();
+
   //Get elements
   var items = document.getElementsByClassName("calendar");
   console.log(items.length);
 
-  //glad date
-  var now = new Date();
-  var month = now.getMonthText();
-  var year = now.getFullYear();
-  var firstDay = new Date(year,now.getMonth(),1);
-  var days = new Date(year, now.getMonth(),0);
-  //alert(firstDay.getDay());
+  //Get the Number of days
+  var month = date.getMonthText();
+  var year = date.getFullYear();
+  var days = new Date(year, date.getMonth(),0);
 
   //Create the HTML -- Month
-  text = "<div class = 'month'><ul><li class='prev'>&#10094;</li><li class='month_year'>"+month+"<br><span style='font-size:18px'>"+year+"</span></li><li class='next'>&#10095;</li></ul></div>"
+  text = "<div class = 'month'><ul><li class='prev' onclick='prev_month();''>&#10094;</li><li class='month_year'>"+month+"<br><span style='font-size:18px'>"+year+"</span></li><li class='next' onclick='next_month()'>&#10095;</li></ul></div>"
 
   //Table Header
   text = text + "<table><tr><th>Mo</th><th>Tu</th><th>We</th><th>Th</th><th>Fr</th><th>Sa</th><th>Su</th></tr>"
@@ -27,7 +46,7 @@ function create_calendar(){
   //Fill Array
   for(i = 0; i < days.getDate(); i++){
     //console.log(i);
-    calendarDays[i+firstDay.getDay()] = i + 1;
+    calendarDays[i+date.getDay()] = i + 1;
   }
 
   days_text = " ";
@@ -42,7 +61,7 @@ function create_calendar(){
     }
     else{
       console.log(calendarDays[i]);
-       days_text = days_text + "<td class='day'>" + calendarDays[i] + "</td>";
+       days_text = days_text + "<td class='day' id='day"+ calendarDays[i] +"'>" + calendarDays[i] + "</td>";
       //days_text = days_text + "<td class='day>"+calendarDays[i] +"</td>";
     }
     //days_text = days_text + "<td>test</td>";
@@ -61,12 +80,28 @@ function create_calendar(){
 
   //Add to the text
   items[0].innerHTML = text;
+}
 
+function reset_days(){
+  calendarDays = Array(42).fill(0);
+}
 
+function next_month(){
+  //alert("Next Month");
+  date.setMonth(date.getMonth() + 1);
+  create_calendar();
+}
 
+function prev_month(){
+  //alert("Prev Month");
+  date.setMonth(date.getMonth() - 1);
+  create_calendar();
 }
 
 
+
+
+
 window.onload = function(){
-  window.document.body.onload = create_calendar(); // note removed parentheses
+  window.document.body.onload = setup(); // note removed parentheses
 };
