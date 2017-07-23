bb1d3f821ad807f73302782d8dda937e653109b8
Miles Shipman <mshipman3@gatech.edu>
2017-07-23 15:02:54 -0400

    Fixed Arrow position and added active for 'todays date'

--- js/calendar.js
+++ js/calendar.js
@@ -30,7 +30,7 @@ function create_calendar(){
 
   //Get elements
   var items = document.getElementsByClassName("calendar");
-  console.log(items.length);
+  //console.log(items.length);
 
   //Get the Number of days
   var month = date.getMonthText();
@@ -39,7 +39,7 @@ function create_calendar(){
 
   //Create the HTML -- Month
   //Turn this into a table it's just gonna be easier
-  text = "<div class = 'month'><ul><li class='prev' onclick='prev_month();''>&#10094;</li><li class='month_year'>"+month+"<br><span style='font-size:18px'>"+year+"</span></li><li class='next' onclick='next_month()'>&#10095;</li></ul></div>"
+  text = "<div class = 'month'><ul><li class='prev' onclick='prev_month();''>&#10094;</li><li style='width:80%;' class='month_year'>"+month+"<br><span style='font-size:18px'>"+year+"</span></li><li class='next' onclick='next_month()'>&#10095;</li></ul></div>"
 
   //Table Header
   text = text + "<table><tr><th>Mo</th><th>Tu</th><th>We</th><th>Th</th><th>Fr</th><th>Sa</th><th>Su</th></tr>"
@@ -60,32 +60,34 @@ function create_calendar(){
         days_text = days_text + "<tr>";
       }
     }
-    console.log("Test");
+    //console.log("Test");
     //add the space
     if(calendarDays[i] == 0){
       days_text = days_text + "<td class='day'></td>";
     }
     else{
-      console.log(calendarDays[i]);
+      //console.log(calendarDays[i]);
        days_text = days_text + "<td class='day' id='day"+ calendarDays[i] +"'>" + calendarDays[i] + "</td>";
       //days_text = days_text + "<td class='day>"+calendarDays[i] +"</td>";
     }
     //days_text = days_text + "<td>test</td>";
 
-   console.log(i%7);
+   //console.log(i%7);
     if( i % 7 == 6){
       days_text = days_text + "</tr>";
     }
     
   }
 
-  console.log(calendarDays);
+  //console.log(calendarDays);
 
   //End table
   text = text + days_text + "</table>";
 
   //Add to the text
   items[0].innerHTML = text;
+
+  set_active();
 }
 
 function reset_days(){
@@ -102,6 +104,17 @@ function prev_month(){
   //alert("Prev Month");
   date.setMonth(date.getMonth() - 1);
   create_calendar();
+}
+
+function set_active(){
+  var today_date = new Date();
+
+  if(today_date.getMonth() == date.getMonth() && today_date.getFullYear() == date.getFullYear()){
+    var class_date = "day" + today_date.getDate();
+    var item = document.getElementById(class_date);
+
+    item.classList.add("active");
+  }
 }
 
 
